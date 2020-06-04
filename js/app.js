e8687043d35baf60061d26286ba4cb15f2a6a1c1
ivandi1980 <ivandi.djoh@gmail.com>
2020-06-05 03:02:01 +0700

    javascript added

---
+++ js/app.js
@@ -0,0 +1,27 @@
+TweenMax.defaultEase = Linear.easeOut;
+
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  navigation: true,
+  onLeave: (origin, destination, direction) => {
+    const section = destination.item;
+    const title = section.querySelector("h1");
+    const tl = new TimelineMax({ delay: 0.5 });
+    tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
+    if (destination.index === 1) {
+      const chairs = document.querySelectorAll(".chair");
+      const description = document.querySelector(".description");
+      tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-10%" })
+        .fromTo(
+          description,
+          0.5,
+          { opacity: 0, y: "50" },
+          { y: "0", opacity: 1 }
+        )
+        .fromTo(chairs[0], 1, { opacity: 1 }, { opacity: 1 })
+        .fromTo(chairs[1], 1, { opacity: 0 }, { opacity: 1 })
+        .fromTo(chairs[2], 1, { opacity: 0 }, { opacity: 1 });
+    }
+  }
+});
